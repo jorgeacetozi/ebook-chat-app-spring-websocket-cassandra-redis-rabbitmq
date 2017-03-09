f905be3cb3cd479ca8709aae6bfd698feaeff966
Jorge Acetozi <jorge.acetozi@gmail.com>
2017-03-09 18:39:49 -0300

    Create unit tests for Destinations utility class

---
+++ ebook-chat/src/test/java/br/com/jorgeacetozi/ebookChat/utils/DestinationsTest.java
@@ -0,0 +1,26 @@
+package br.com.jorgeacetozi.ebookChat.utils;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import org.junit.Test;
+
+public class DestinationsTest {
+	
+	private final String chatRoomId = "123";
+	
+	@Test
+	public void shouldGetPublicMessagesDestination() {
+		assertThat(Destinations.ChatRoom.publicMessages("123"), is("/topic/" + chatRoomId + "/public-messages"));
+	}
+
+	@Test
+	public void shouldGetPrivateMessagesDestination() {
+		assertThat(Destinations.ChatRoom.privateMessages("123"), is("/queue/" + chatRoomId + "/private-messages"));
+	}
+	
+	@Test
+	public void shouldGetConnectedUsersDestination() {
+		assertThat(Destinations.ChatRoom.connectedUsers("123"), is("/topic/" + chatRoomId + "/connected-users"));
+	}
+}
