8354f2900aaa0f77b8ed22943fedd7d85d8fc69e
Jorge Acetozi <jorge.acetozi@gmail.com>
2017-03-09 18:46:03 -0300

    Create unit tests for SystemMessages utility class

---
+++ ebook-chat/src/test/java/br/com/jorgeacetozi/ebookChat/utils/SystemMessagesTest.java
@@ -0,0 +1,39 @@
+package br.com.jorgeacetozi.ebookChat.utils;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.Is.is;
+
+import org.junit.Test;
+
+import br.com.jorgeacetozi.ebookChat.chatroom.domain.model.InstantMessage;
+
+public class SystemMessagesTest {
+	
+	private final String chatRoomId = "123";
+	private final String username = "jorge_acetozi";
+
+	@Test
+	public void shouldGetWelcomeInstantMessage() {
+		InstantMessage welcomeMessage = SystemMessages.welcome(chatRoomId, username);
+		
+		assertThat(welcomeMessage.isPublic(), is(true));
+		assertThat(welcomeMessage.isFromAdmin(), is(true));
+		assertThat(welcomeMessage.getChatRoomId(), is(chatRoomId));
+		assertThat(welcomeMessage.getFromUser(), is(SystemUsers.ADMIN.getUsername()));
+		assertThat(welcomeMessage.getToUser(), is(nullValue()));
+		assertThat(welcomeMessage.getText(), is(username + " joined us :)"));
+	}
+	
+	@Test
+	public void shouldGetGoodbyeInstantMessage() {
+		InstantMessage goodbyeMessage = SystemMessages.goodbye(chatRoomId, username);
+		
+		assertThat(goodbyeMessage.isPublic(), is(true));
+		assertThat(goodbyeMessage.isFromAdmin(), is(true));
+		assertThat(goodbyeMessage.getChatRoomId(), is(chatRoomId));
+		assertThat(goodbyeMessage.getFromUser(), is(SystemUsers.ADMIN.getUsername()));
+		assertThat(goodbyeMessage.getToUser(), is(nullValue()));
+		assertThat(goodbyeMessage.getText(), is(username + " left us :("));
+	}
+}
