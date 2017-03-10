6ec4bd5d76ca77581dbe0b51f18c7b5c63002bd3
Jorge Acetozi <jorge.acetozi@gmail.com>
2017-03-10 20:42:07 -0300

    Create websocket automatic reconnect feature on client side

--- ebook-chat/src/main/java/br/com/jorgeacetozi/ebookChat/chatroom/api/ChatRoomController.java
+++ ebook-chat/src/main/java/br/com/jorgeacetozi/ebookChat/chatroom/api/ChatRoomController.java
@@ -1,12 +1,14 @@
 package br.com.jorgeacetozi.ebookChat.chatroom.api;
 
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,21 +20,22 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.jorgeacetozi.ebookChat.chatroom.domain.model.ChatRoom;
+import br.com.jorgeacetozi.ebookChat.chatroom.domain.model.ChatRoomUser;
 import br.com.jorgeacetozi.ebookChat.chatroom.domain.model.InstantMessage;
 import br.com.jorgeacetozi.ebookChat.chatroom.domain.service.ChatRoomService;
 import br.com.jorgeacetozi.ebookChat.chatroom.domain.service.InstantMessageService;
 
 @Controller
 public class ChatRoomController {
-	
+
 	@Autowired
 	private ChatRoomService chatRoomService;
-	
+
 	@Autowired
 	private InstantMessageService instantMessageService;
-	
+
 	@Secured("ROLE_ADMIN")
-	@RequestMapping(path="/chatroom", method=RequestMethod.POST)
+	@RequestMapping(path = "/chatroom", method = RequestMethod.POST)
 	@ResponseBody
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public ChatRoom createChatRoom(@RequestBody ChatRoom chatRoom) {
@@ -40,19 +43,32 @@ public class ChatRoomController {
 	}
 
 	@RequestMapping("/chatroom/{chatRoomId}")
-	public ModelAndView join(@PathVariable String chatRoomId, Principal principal){
+	public ModelAndView join(@PathVariable String chatRoomId, Principal principal) {
 		ModelAndView modelAndView = new ModelAndView("chatroom");
 		modelAndView.addObject("chatRoom", chatRoomService.findById(chatRoomId));
-		modelAndView.addObject("oldMessages", instantMessageService.findAllInstantMessagesFor(principal.getName(), chatRoomId));
 		return modelAndView;
 	}
-	
+
+	@SubscribeMapping("/connected-users")
+	public List<ChatRoomUser> listChatRoomConnectedUsersOnSubscribe(SimpMessageHeaderAccessor headerAccessor) {
+		String chatRoomId = headerAccessor.getSessionAttributes().get("chatRoomId").toString();
+		return chatRoomService.findById(chatRoomId).getConnectedUsers();
+	}
+
+	@SubscribeMapping("/old-messages")
+	public List<InstantMessage> listOldMessagesFromUserOnSubscribe(Principal principal,
+			SimpMessageHeaderAccessor headerAccessor) {
+		String chatRoomId = headerAccessor.getSessionAttributes().get("chatRoomId").toString();
+		return instantMessageService.findAllInstantMessagesFor(principal.getName(), chatRoomId);
+	}
+
 	@MessageMapping("/send-message")
-	public void sendMessage(@Payload InstantMessage instantMessage, Principal principal, SimpMessageHeaderAccessor headerAccessor) {
+	public void sendMessage(@Payload InstantMessage instantMessage, Principal principal,
+			SimpMessageHeaderAccessor headerAccessor) {
 		String chatRoomId = headerAccessor.getSessionAttributes().get("chatRoomId").toString();
 		instantMessage.setFromUser(principal.getName());
 		instantMessage.setChatRoomId(chatRoomId);
-		
+
 		if (instantMessage.isPublic()) {
 			chatRoomService.sendPublicMessage(instantMessage);
 		} else {
